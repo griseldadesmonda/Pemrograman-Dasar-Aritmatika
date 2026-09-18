5b50ac7115fd3a3625cd714d6c29d2d92c2c7e72
griseldadesmonda <griseldadesmonda.pdf@gmail.com>
2026-09-18 21:48:37 +0800

    Add files via upload

---
+++ Variabel004.java
@@ -0,0 +1,15 @@
+public class Variabel004 {
+    public static void main(String[] args) {
+        int Bilangan1, Bilangan2;
+
+        Bilangan1 = 21;
+        Bilangan2 = 7;
+
+        System.out.println("Bilangan 1 = " + Bilangan1);
+        System.out.println("Bilangan 2 = " + Bilangan2);
+        System.out.println("Hasil Pembagian : 21 / 7 =  " + Bilangan1 / Bilangan2);
+        System.out.println("Hasil Perkalian : 21 x 7 = " + Bilangan1 * Bilangan2);
+        System.out.println("Hasil Pengurangan : 21 - 7 = " + (Bilangan1 - Bilangan2));
+        System.out.println("Hasil Penjumlahan : 21 + 7 = " + (Bilangan1 + Bilangan2));
+    }
+}
